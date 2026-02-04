4c3e023732744122b728f43c910452a415b84b6b
pradip2059 <pradipsapkota2059@gmail.com>
2026-02-04 09:49:27 -0600

    Create FinancialAdvisor.java

---
+++ src/main/java/com/wellsfargo/counselor/entity/FinancialAdvisor.java
@@ -0,0 +1,50 @@
+package com.wellsfargo.counselor.entities;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "financial_advisor")
+public class FinancialAdvisor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "advisor_id")
+    private Long advisorId;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "advisor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Client> clients;
+
+    protected FinancialAdvisor() { }
+
+    public FinancialAdvisor(String firstName, String lastName, String email, List<Client> clients) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.clients = (clients != null) ? clients : new ArrayList<>();
+    }
+
+    public Long getAdvisorId() { return advisorId; }
+
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public List<Client> getClients() { return clients; }
+    public void setClients(List<Client> clients) { this.clients = clients; }
+}
